d93b5bd52e654c9aa5ad98bd071bfd9868084086
CURVX <chakrawarti.anupam@gmail.com>
2021-04-22 16:17:09 +0530

    Barebone implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import text from "./components/data";
+import Paragraph from "./components/Paragraph";
 
 function App() {
+  const textData = text;
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setCount(value);
+    // console.log(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let amount = Number(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 9) {
+      amount = 9;
+    }
+    const newTextData = textData.slice(0, amount);
+    setData(newTextData);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Paragraphs</label>
+        <input type="number" value={count} onChange={handleChange} />
+        <button type="submit">GENERATE</button>
+      </form>
+      {data.map((item, index) => {
+        return <Paragraph key={index} item={item} />;
+      })}
+    </>
   );
 }
 
